cb8a790702f68a92e46a776cac6377534e12874a
olympiawoj <owojcik7@gmail.com>
2021-02-27 20:06:34 -0500

    useTrivia Custom React Hook

--- src/App.js
+++ src/App.js
@@ -3,45 +3,38 @@ import Question from "./components/Question";
 import CategorySelector from "./components/CategorySelector";
 import ResultModal from "./components/ResultModal";
 import Scoreboard from "./components/Scoreboard";
+import useTrivia from "./useTrivia";
 import "./App.css";
 
 export default function App() {
-  const [question, setQuestion] = useState();
-  const [selectedCategory, setSelectedCategory] = useState("any");
-  const [isCorrect, setIsCorrect] = useState(null) // no answer yet so not wrong, not right
+  const { question, getQuestion, category, setCategory } = useTrivia();
+  const [isCorrect, setIsCorrect] = useState(null); // no answer yet so not wrong, not right
 
-  const getQuestion = useCallback(() => {
-    setIsCorrect(null)
-    let url = "https://opentdb.com/api.php?amount=1";
-    if (selectedCategory !== "any") url += `&category=${selectedCategory}`;
-
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setQuestion(data.results[0]));
-  }, [selectedCategory]);
-
-  useEffect(() => {
-    getQuestion();
-  }, [getQuestion, selectedCategory]);
-
-  function handleQuestionAnswered(answer){
+  function handleQuestionAnswered(answer) {
     const isAnswerCorrect = answer === question.correct_answer;
-    setIsCorrect(isAnswerCorrect)
+    setIsCorrect(isAnswerCorrect);
+  }
 
+  function handleNextQuestion() {
+    setIsCorrect(null);
+    getQuestion();
   }
 
   return (
     <div className="app">
       {/* show the result modal ----------------------- */}
-     {isCorrect !== null &&  <ResultModal isCorrect={isCorrect} question={question} getQuestion={getQuestion}/>}
+      {isCorrect !== null && (
+        <ResultModal
+          isCorrect={isCorrect}
+          question={question}
+          getQuestion={handleNextQuestion}
+        />
+      )}
 
       {/* question header ----------------------- */}
       <div className="question-header">
-        <CategorySelector
-          category={selectedCategory}
-          chooseCategory={setSelectedCategory}
-        />
-        <Scoreboard isCorrect={isCorrect}/>
+        <CategorySelector category={category} chooseCategory={setCategory} />
+        <Scoreboard isCorrect={isCorrect} />
       </div>
 
       {/* the question itself ----------------------- */}
@@ -56,7 +49,7 @@ export default function App() {
 
       {/* question footer ----------------------- */}
       <div className="question-footer">
-        <button>Go to next question 👉</button>
+        <button onClick={handleNextQuestion}>Go to next question 👉</button>
       </div>
     </div>
   );
